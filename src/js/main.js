de749cfd7d7d41c7ac2f12b6451a74541f03411e
Juferis <66511872+Juferis@users.noreply.github.com>
2021-06-11 11:53:35 +0900

    리스트 생성 및 삭제 pt.3
    리스트 각각 내용 추가 기능

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,56 @@ const addBtn = document.getElementById("jsBtn");
 const listMain = document.getElementById("jsList");
 const mainDiv = document.getElementById("mainDiv");
 
+let INPUT_VALUE = "";
+
 let divArr = [];
 let formArr = [];
 let listArr = [];
 
+function handleDelete(event) {
+  event.preventDefault();
+  const delBtn = event.target;
+  const li = delBtn.parentNode;
+  const ul = li.parentNode;
+  ul.removeChild(li);
+}
+
+function paintList(parentDiv) {
+  const ul = parentDiv.lastChild;
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("button");
+  ul.appendChild(li);
+  span.innerText = `${INPUT_VALUE}`;
+  delBtn.innerText = "삭제";
+  delBtn.id = "jsDelete";
+  delBtn.addEventListener("click", handleDelete);
+  li.appendChild(span);
+  li.appendChild(delBtn);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  INPUT_VALUE = event.target.querySelector("input").value;
+  const parentDiv = event.target.parentNode;
+  paintList(parentDiv);
+}
+
 function createInput(form) {
+  // input 생성
   const input = document.createElement("input");
   const numId = listArr.length;
-  input.placeholder = "Add a list";
+  input.placeholder = "Write a list";
   input.id = `jsInput_${numId}`;
   input.className = "list__input";
   input.type = "text";
   listArr.push(input);
   form.appendChild(input);
+  form.addEventListener("submit", handleSubmit);
 }
 
 function createFormUl(div) {
+  // form 생성
   const form = document.createElement("form");
   const ul = document.createElement("ul");
   const formId = formArr.length;
@@ -32,7 +66,6 @@ function createFormUl(div) {
 }
 
 function handleClickAddBtn() {
-  event.preventDefault();
   const div = document.createElement("div");
   const divNum = divArr.length;
   div.id = `jsDiv_${divNum}`;
